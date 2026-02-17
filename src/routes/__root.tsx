ad75294a66e9b56fd653babadc5c631d3e353bb6
Nazar Kundys <nazarrisd@gmail.com>
2026-02-17 14:51:21 +0200

    ♻️ refactor(navbar): restructure navbar component into modular architecture

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import Navbar from '@/components/features/Navbar';
+import { Navbar } from '@/components/organisms/Navbar';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
