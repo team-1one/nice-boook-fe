6b99d912e43d9239a8deb48858e82ac3a32fd127
Dmytro <dimusgamer2.0@gmail.com>
2026-02-21 21:19:19 +0200

    refactor(scrum-5): decompose BookImage component
    refactor(scrum-5): decompose BookImage component, preventDefault function, rename states, change state type, extract ebookFrame to consts

--- src/components/books/BookCard.tsx
+++ src/components/books/BookCard.tsx
@@ -5,95 +5,47 @@ import {
   CardHeader,
 } from '@/components/ui/card';
 import { ButtonGroup } from '@/components/ui/button-group';
-import { Headphones, Heart, Van } from 'lucide-react';
+import { cn, withPreventDefault } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Link } from '@tanstack/react-router';
 import type { BookData } from '@/types/book';
 import type { ComponentProps } from 'react';
 import phrases from '@/constants/phrases';
+import { Heart, Van } from 'lucide-react';
+import BookImage from './BookImage';
 import { useState } from 'react';
-import { cn } from '@/lib/utils';
+
+interface BookCardState {
+  isInCart: boolean;
+  isWishlisted: boolean;
+}
 
 interface BookCardProps {
   cardProps?: ComponentProps<typeof Card>;
   product: BookData;
 }
 
-const handleCardActionButtonClick = (
-  event: React.MouseEvent<HTMLButtonElement>,
-  callback: () => void,
-) => {
-  event.preventDefault();
-  callback();
-};
-
-const ebookFrame =
-  'https://cxqrvyjozjyswjemkfhk.supabase.co/storage/v1/object/public/books/img/audiobook/2.webp';
-
 // TODO: Implement currency conversion and localization for price display
 // TODO: Remove hardcoded colors, sizes etc. from classnames
 
 export const BookCard = ({ cardProps, product }: BookCardProps) => {
-  const [isWishlisted, setIsWishlisted] = useState(false);
-  const [isLiked, setIsLiked] = useState(false);
+  // TODO: use global state management for cart and wishlist status instead of local state in component
+  const [{ isInCart, isWishlisted }, setState] = useState<BookCardState>({
+    isInCart: false,
+    isWishlisted: false,
+  });
 
-  // TODO: Here we need some more clear way to understand if we need icon
-  const isEbook = product.type === 'kindle';
-  const isAudioBook = product.type === 'audiobook';
-
-  const toggleLike = () => {
-    setIsLiked((prev) => !prev);
+  const toggleStatus = (statusKey: keyof BookCardState) => {
+    setState((prev) => ({ ...prev, [statusKey]: !prev[statusKey] }));
   };
 
-  const toggleWishlist = () => {
-    setIsWishlisted((prev) => !prev);
-  };
-
-  const wishlistButtonLabel =
-    isWishlisted ? phrases.addedToCart : phrases.addToCart;
+  const cartButtonLabel = isInCart ? phrases.addedToCart : phrases.addToCart;
 
   return (
     // FIXME: Fix hardcoded path type assertion
     <Link to={`/books/${product.slug}` as '/paper'}>
       <Card {...cardProps}>
-        <div className="relative overflow-hidden shrink-0">
-          <div className="w-full aspect-square relative bg-gray-50">
-            {isEbook ?
-              <>
-                <div className="absolute inset-[9%] inset-x-[14%] flex items-center justify-center overflow-hidden">
-                  <img
-                    src={'https://placehold.co/320x320'}
-                    alt={product.name}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-                  <img
-                    src={ebookFrame}
-                    alt="Ebook Frame"
-                    className="w-full h-full object-contain"
-                  />
-                </div>
-              </>
-            : <div className="flex items-center justify-center w-full h-full">
-                <img
-                  src={'https://placehold.co/320x320'}
-                  alt={product.name}
-                  className="w-full h-full object-contain"
-                />
-              </div>
-            }
-          </div>
-
-          {isAudioBook && (
-            <div className="absolute top-1 right-4 bg-green-500 rounded-full p-1 size-10 flex items-center justify-center">
-              <Headphones
-                color="#fff"
-                size={18}
-              />
-            </div>
-          )}
-        </div>
+        <BookImage book={product} />
 
         <CardHeader className="gap-1">
           <h5 className="font-medium">{product.name}</h5>
@@ -101,20 +53,18 @@ export const BookCard = ({ cardProps, product }: BookCardProps) => {
         </CardHeader>
 
         <CardContent>
-          <div>
-            <div className="gap-2 mb-2 flex items-center">
-              <h3 className="font-semibold">{`₴${product.priceDiscount ?? product.priceRegular}`}</h3>
-              {product.priceDiscount && (
-                <h4 className="line-through text-sm text-muted-foreground">{`₴${product.priceRegular}`}</h4>
-              )}
-            </div>
+          <div className="gap-2 mb-2 flex items-center">
+            <h3 className="font-semibold">{`₴${product.priceDiscount ?? product.priceRegular}`}</h3>
+            {product.priceDiscount && (
+              <h4 className="line-through text-sm text-muted-foreground">{`₴${product.priceRegular}`}</h4>
+            )}
+          </div>
 
-            <div className="flex items-center gap-1">
-              <Van color="#27AE60" />
-              <span className="text-sm text-[#27AE60] font-bold">
-                {phrases.inStock}
-              </span>
-            </div>
+          <div className="flex items-center gap-1">
+            <Van color="#27AE60" />
+            <span className="text-sm text-[#27AE60] font-bold">
+              {phrases.inStock}
+            </span>
           </div>
         </CardContent>
 
@@ -122,26 +72,26 @@ export const BookCard = ({ cardProps, product }: BookCardProps) => {
           <ButtonGroup className="w-full">
             <ButtonGroup className="flex-1">
               <Button
-                variant={isWishlisted ? 'outline' : 'default'}
-                size={'xl'}
-                className={cn('w-full transition-all duration-300', {
-                  'text-[#27AE60] hover:text-[#27AE60]/80': isWishlisted,
+                variant={isInCart ? 'outline' : 'default'}
+                size="xl"
+                className={cn('w-full transition-all', {
+                  'text-[#27AE60] hover:text-[#27AE60]/80': isInCart,
                 })}
-                onClick={(e) => handleCardActionButtonClick(e, toggleWishlist)}
+                onClick={withPreventDefault(() => toggleStatus('isInCart'))}
               >
-                {wishlistButtonLabel}
+                {cartButtonLabel}
               </Button>
             </ButtonGroup>
 
             <ButtonGroup>
               <Button
-                variant={'outline'}
-                size={'icon-xl'}
-                onClick={(e) => handleCardActionButtonClick(e, toggleLike)}
+                variant="outline"
+                size="icon-xl"
+                onClick={withPreventDefault(() => toggleStatus('isWishlisted'))}
               >
                 <Heart
-                  fill={isLiked ? 'red' : 'none'}
-                  strokeWidth={isLiked ? 0 : 2}
+                  fill={isWishlisted ? 'red' : 'none'}
+                  strokeWidth={isWishlisted ? 0 : 2}
                 />
               </Button>
             </ButtonGroup>
