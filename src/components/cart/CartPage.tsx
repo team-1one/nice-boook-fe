545ce529100861352038b612f584f7bfd4ef4c20
Oksana Kyreieva <senyakir@gmail.com>
2026-02-26 01:37:57 +0200

    Implement empty cart state with early return

--- src/components/cart/CartPage.tsx
+++ src/components/cart/CartPage.tsx
@@ -3,11 +3,16 @@ import { CartItem } from "./CartItem";
 import { CartSummary } from "./CartSummary";
 import { BackButton } from "./BackButton";
 import { useCartStore } from "./cart.store";
+import { EmptyCart } from "./EmptyCart";
 
 const CONTAINER_STYLES = "mx-auto max-w-[1440px] mt-6 sm:mt-16 px-4 sm:px-6 lg:px-8 xl:px-[152px] mb-20";
 
 export function CartPage() {
   const items = useCartStore((state) => state.items)
+
+  if (items.length === 0) {
+    return <EmptyCart />
+  }
 
   return (
     <main className={CONTAINER_STYLES}>
